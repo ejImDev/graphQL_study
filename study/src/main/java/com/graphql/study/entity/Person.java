0959773114403ba880b70fc07f37848aa059c196
ejim0093 <ejim0093@gmail.com>
2023-05-31 00:20:51 +0900

    ex01) person,address

---
+++ study/src/main/java/com/graphql/study/entity/Person.java
@@ -0,0 +1,35 @@
+package com.graphql.study.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "addressId", referencedColumnName = "id")
+    private Address address;
+
+    public Person(String firstName, String lastName, String phoneNumber, String email, Address address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+    }
+}
